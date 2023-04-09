bbd82adb5946cac52f1ce68b037bd49545484ed5
anighosal <121676026+anighosal@users.noreply.github.com>
2023-04-10 02:46:56 +0600

    nav added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>header</h1>
+        <div className='flex items-center justify-between px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg-gray-100'>
+          <Link>   <span className='font-extrabold text-3xl  bg-blend-color-burn '>BuildCareer</span>
+          </Link>
+          <ul className='items-center hidden space-x-8 lg:flex'>
+            <li>    <NavLink
+              to='/'
+              aria-label='Home'
+              title='Home'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Home
+            </NavLink> </li>
+            <li>    <NavLink
+              to='/statistics'
+              aria-label='Statistics'
+              title='Statistics'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Statistics
+            </NavLink> </li>
+            <li>    <NavLink
+              to='/appliedJobs'
+              aria-label='AppliedJobs'
+              title='AppliedJobs'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+             AppliedJobs
+            </NavLink> </li>
+            <li>    <NavLink
+              to='/blog'
+              aria-label='Blog'
+              title='Blog'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+             Blog
+            </NavLink> </li>
+          </ul>
+          <Link>
+          <button>Star Apply</button>
+          </Link>
+
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blog from './components/Blog/Blog';
 
 
 
@@ -14,6 +17,24 @@ const router =createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    children: [
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path:'statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path:'appliedjobs',
+        element:< AppliedJobs></AppliedJobs>
+      },
+      {
+        path:'blog',
+        element:<Blog></Blog>
+      }
+    ]
    
   }
 ])
