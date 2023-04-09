778479d38bd870e6e3433cac147108ead8b9d42c
anighosal <121676026+anighosal@users.noreply.github.com>
2023-04-10 00:07:30 +0600

    header footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-         <h2>hello careerBuild</h2>
+     <Header></Header>
+     <Outlet></Outlet>
+     <Footer></Footer>
+    
     </div>
   )
 }
